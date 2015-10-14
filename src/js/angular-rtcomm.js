8112dd1227c14f7c9fa481e9b8e55d4765fe0ef7
Scott W Graham <swgraham@us.ibm.com>
2015-10-14 10:01:13 -0600

    Add missing file

---
+++ src/js/angular-rtcomm.js
@@ -0,0 +1,11 @@
+/*
+ * This is the main angular-rtcomm module.
+ */
+(function() {
+  angular.module('angular-rtcomm', [
+    'ui.bootstrap',
+    'treeControl',
+    'angular-rtcomm-ui',
+    'angular-rtcomm-service'
+  ]);
+})();
